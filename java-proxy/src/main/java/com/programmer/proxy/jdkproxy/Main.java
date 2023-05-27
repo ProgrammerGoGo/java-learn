f07d2a0782b967c74f0183233b6bac89eb8ce4b3
ProgrammerGoGo <98639494+ProgrammerGoGo@users.noreply.github.com>
2023-05-27 15:30:13 +0800

    Update Main.java

--- java-proxy/src/main/java/com/programmer/proxy/jdkproxy/Main.java
+++ java-proxy/src/main/java/com/programmer/proxy/jdkproxy/Main.java
@@ -6,9 +6,16 @@ package com.programmer.proxy.jdkproxy;
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         UserService target = new UserServiceImpl();
         UserService proxy = (UserService) UserProxy.getProxy(target);
         proxy.save();
+        
+        // 这里我们将jdk生成的代理类输出了出来，方便后面分析使用
+        byte[] bytes = ProxyGenerator.generateProxyClass("$Proxy0", new Class[]{target.getClass()});
+
+        FileOutputStream os = new FileOutputStream("Proxy0.class");
+        os.write(bytes);
+        os.close();
     }
 }
